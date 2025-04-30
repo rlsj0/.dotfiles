539425c84c6180c7b0ecf6f2e87155b7b619042f
rlsj0 <rodrileja@gmail.com>
2025-04-30 12:56:51 +0200

    fix nvim

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+-- Custom features for markdown files
+
+-- textwidth and autoindent seem to work with vim.opt_local
+vim.opt_local.textwidth = 70
+vim.opt_local.autoindent = true
+
+-- Fold variables
+
+vim.o.foldlevelstart = 1
+vim.o.foldlevel = 1
---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,9 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
+  { 'rebelot/kanagawa.nvim' },
+  { 'ronisbr/nano-theme.nvim' },
+}
