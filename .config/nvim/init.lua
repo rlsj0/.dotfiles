590a005344f35436e84b03a2cc85cadb061aaf3b
rlsj0 <rodrileja@gmail.com>
2026-04-09 15:48:19 +0200

    add: modificando nvim para jdtls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -659,13 +659,26 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      -- require('mason-lspconfig').setup {
+      --   handlers = {
+      --     function(server_name)
+      --       local server = servers[server_name] or {}
+      --       -- This handles overriding only values explicitly passed
+      --       -- by the server configuration above. Useful when disabling
+      --       -- certain features of an LSP (for example, turning off formatting for ts_ls)
+      --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      --       require('lspconfig')[server_name].setup(server)
+      --     end,
+      --   },
+      -- }
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
+            if server_name == 'jdtls' then
+              -- Skip jdtls, we'll handle it with nvim-jdtls
+              return
+            end
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
@@ -708,6 +721,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        java = { 'google-java-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -6,4 +6,39 @@ return {
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
   { 'rebelot/kanagawa.nvim' },
   { 'ronisbr/nano-theme.nvim' },
+  {
+    'mfussenegger/nvim-jdtls',
+    ft = 'java',
+    dependencies = { 'neovim/nvim-lspconfig' },
+    config = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'java',
+        callback = function()
+          local jdtls = require 'jdtls'
+          local mason_path = vim.fn.stdpath 'data' .. '/mason/packages/jdtls'
+
+          local jdtls_cmd = mason_path .. '/bin/jdtls'
+          local lombok_jar = mason_path .. '/lombok.jar'
+
+          local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+          local workspace_dir = vim.fn.expand '~/.cache/jdtls-workspace/' .. project_name
+
+          local root_dir = jdtls.setup.find_root { '.git', 'mvnw', 'gradlew', 'pom.xml' } or vim.fn.getcwd()
+
+          local config = {
+            cmd = {
+              jdtls_cmd,
+              -- '-javaagent:' .. lombok_jar,
+              '--jvm-arg=-javaagent:' .. lombok_jar,
+              '-data',
+              workspace_dir,
+            },
+            root_dir = root_dir,
+          }
+
+          jdtls.start_or_attach(config)
+        end,
+      })
+    end,
+  },
 }
